ef64908fbb958662a7bef69f6cde1a94a1f2906c
balajipatil0009 <balajipatil20030@gmail.com>
2023-03-18 18:50:57 +0530

    modify 1.1

--- conn.js
+++ conn.js
@@ -1,17 +1,18 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-dotenv.config({path:'./.env'});
-
-
+dotenv.config({ path: "./.env" });
 
 const db = process.env.DATABASE;
 
-mongoose.connect(db,{
- useNewUrlParser: true,
- useUnifiedTopology: true
-}).then(()=>{
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("db connected");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
--- server.js
+++ server.js
@@ -1,39 +1,42 @@
-const { json } = require('express');
-const express = require('express');
-const User = require('./User.js');
+const { json } = require("express");
+const express = require("express");
+const User = require("./User.js");
 
 const app = express();
 
 const PORT = process.env.PORT || 8001;
 
-require('./conn');
+require("./conn");
 
-require('./User.js');
+require("./User.js");
 
 app.use(express.json());
 
-app.use(express.static('build'));
+app.use(express.static("build"));
 
-app.post("/login",(req, res)=>{
-  const{name, password }= req.body;
-  if(!name || !password){
-     res.status(422).json({error: "all Fuields are required"});
-  }else{
-  const user = new User({name, password});
-  user.save().then(()=>{
-    console.log("saved");
-  }).catch((err)=>{
-    res.status(422).json({error: "technical issue"});
-    console.log(err);
-  });
-  res.send(req.body);
-}
+app.post("/login", (req, res) => {
+  const { name, password } = req.body;
+  if (!name || !password) {
+    res.status(422).json({ error: "all Fuields are required" });
+  } else {
+    const user = new User({ name, password });
+    user
+      .save()
+      .then(() => {
+        console.log("saved");
+      })
+      .catch((err) => {
+        res.status(422).json({ error: "technical issue" });
+        console.log(err);
+      });
+    res.send(req.body);
+  }
 });
 
-if(process.env.NODE_ENV == "production" ){
-    app.use(express.static("frontend/onchat-app/build"));
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("frontend/onchat-app/build"));
 }
 
-app.listen(PORT, ()=>{
-    console.log('connected');
-})
+app.listen(PORT, () => {
+  console.log("connected");
+});
